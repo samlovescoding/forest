60034c08f26ebbb222cbf17738e4c7ae5eb1e546
Sampan Verma <himself@samlovescoding.com>
2026-02-06 05:24:51 +0530

    Trust Proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
   )
   ->withMiddleware(function (Middleware $middleware): void {
     $middleware->alias(['layout' => WithLayout::class]);
+    $middleware->trustProxies('*');
   })
   ->withExceptions(function (Exceptions $exceptions): void {
     //
