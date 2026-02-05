d2e15570ed4ee1896d3f50ed5cb8775169d3e3a9
Sampan Verma <himself@samlovescoding.com>
2026-02-06 05:27:15 +0530

    Added envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,46 @@
+@servers(['prolab' => 'sam@192.168.1.100'])
+
+@setup
+  $appDir = '/var/www/celebforest.com';
+  $branch = 'main';
+@endsetup
+
+@story('deploy')
+  pull
+  composer
+  frontend
+  optimize
+  migrate
+@endstory
+
+@task('pull')
+  echo "Pulling latest from {{ $branch }}..."
+  cd {{ $appDir }}
+  git pull origin {{ $branch }}
+@endtask
+
+@task('composer')
+  echo "Installing composer dependencies..."
+  cd {{ $appDir }}
+  composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+@endtask
+
+@task('frontend')
+  echo "Building frontend assets..."
+  cd {{ $appDir }}
+  npm ci
+  npm run build
+@endtask
+
+@task('optimize')
+  echo "Optimizing Laravel..."
+  cd {{ $appDir }}
+  php artisan optimize:clear
+  php artisan optimize
+@endtask
+
+@task('migrate')
+  echo "Running migrations..."
+  cd {{ $appDir }}
+  php artisan migrate --force
+@endtask
