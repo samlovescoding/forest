1074ed12768ddc400ca48a4bfff9122e361f6cfc
Sampan Verma <himself@samlovescoding.com>
2026-02-06 03:36:36 +0530

    Added homepage

--- pages/index.blade.php
+++ pages/index.blade.php
@@ -10,6 +10,18 @@ new class extends Component
 
 <div>
   <title>Welcome</title>
-  <h1 class="text-4xl">samlovescoding/kit</h1>
-  <flux:text>The ULTRA fastest kit for EVERYTHING - Websites, Desktop Apps and Mobile Apps</flux:text>
+  <h1 class="text-4xl">Forest</h1>
+  <flux:text>Best Server for Gooners</flux:text>
+  <flux:text>Better than Discord or Telegram</flux:text>
+
+  <flux:spacer class="h-16" />
+
+  <div class="flex gap-2">
+    <flux:button href="{{ route('login') }}" wire:navigate>
+      Login
+    </flux:button>
+    <flux:button href="{{ route('register') }}" wire:navigate>
+      Register
+    </flux:button>
+  </div>
 </div>
