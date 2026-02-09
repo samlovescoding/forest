a534eb06ba04e64d33cb0cfb675dbf107fec7a19
Sampan Verma <himself@samlovescoding.com>
2026-02-10 04:24:25 +0530

    deploy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,50 +1,58 @@
 @servers(['prolab' => 'sam@192.168.1.100'])
 
 @setup
-  $appDir = '/var/www/celebforest.com';
-  $branch = 'main';
+$appDir = '/var/www/celebforest.com';
+$branch = 'main';
 @endsetup
 
 @story('deploy')
-  pull
-  composer
-  frontend
-  optimize
-  migrate
+pull
+symlinks
+composer
+frontend
+optimize
+migrate
 @endstory
 
 @task('pull')
-  echo "Pulling latest from {{ $branch }}..."
-  cd {{ $appDir }}
-  git fetch origin {{ $branch }}
-  git reset --hard origin/{{ $branch }}
+echo "Pulling latest from {{ $branch }}..."
+cd {{ $appDir }}
+git fetch origin {{ $branch }}
+git reset --hard origin/{{ $branch }}
 @endtask
 
 @task('composer')
-  echo "Installing composer dependencies..."
-  cd {{ $appDir }}
-  composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+echo "Installing composer dependencies..."
+cd {{ $appDir }}
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 @endtask
 
 @task('frontend')
-  echo "Building frontend assets..."
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  cd {{ $appDir }}
-  npm ci
-  npm run build
+echo "Building frontend assets..."
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+cd {{ $appDir }}
+npm ci
+npm run build
 @endtask
 
 
 @task('optimize')
-  echo "Optimizing Laravel..."
-  cd {{ $appDir }}
-  php artisan optimize:clear
-  php artisan optimize
+echo "Optimizing Laravel..."
+cd {{ $appDir }}
+php artisan optimize:clear
+php artisan optimize
 @endtask
 
 @task('migrate')
-  echo "Running migrations..."
-  cd {{ $appDir }}
-  php artisan migrate --force
+echo "Running migrations..."
+cd {{ $appDir }}
+php artisan migrate --force
+@endtask
+
+@task('symlinks')
+echo "Restoring storage symlinks..."
+rm -rf {{ $appDir }}/storage/app/public {{ $appDir }}/storage/app/private
+ln -s /storage/celebforest.com {{ $appDir }}/storage/app/public
+ln -s /data/celebforest.com {{ $appDir }}/storage/app/private
 @endtask
