64212be9b4331577b0024f4935c612b3a5890743
Sampan Verma <himself@samlovescoding.com>
2026-02-10 02:05:30 +0530

    Change Sidebar Icon

--- components/sidebar.blade.php
+++ components/sidebar.blade.php
@@ -26,24 +26,25 @@ new class extends Component
   <flux:sidebar collapsible sticky class="bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
     <flux:sidebar.nav>
       <x-sidebar-item icon="home" href="{{ route('home') }}">Home</x-sidebar-item>
-      <flux:sidebar.group expandable icon="star" heading="Database" class="grid">
+      <flux:sidebar.group expandable icon="circle-stack" heading="Database" class="grid">
         <x-sidebar-item href="{{ route('people.index') }}" matching="start">People</x-sidebar-item>
         <x-sidebar-item href="{{ route('films.index') }}" matching="start">Films</x-sidebar-item>
         <x-sidebar-item href="{{ route('shows.index') }}" matching="start">TV Shows</x-sidebar-item>
       </flux:sidebar.group>
     </flux:sidebar.nav>
-    <flux:sidebar.spacer />
+    <flux:sidebar.spacer/>
     <flux:dropdown position="top" align="start" class="max-lg:hidden">
-      <flux:sidebar.profile avatar="{{ $this->user->picture() }}" name="{{ $this->user->name }}" icon:trailing="chevron-up" />
-      <x-user-menu />
+      <flux:sidebar.profile avatar="{{ $this->user->picture() }}" name="{{ $this->user->name }}"
+                            icon:trailing="chevron-up"/>
+      <x-user-menu/>
     </flux:dropdown>
   </flux:sidebar>
   <flux:header class="lg:hidden">
-    <flux:sidebar.toggle class="lg:hidden" icon="bars-3" inset="left" />
-    <flux:spacer />
+    <flux:sidebar.toggle class="lg:hidden" icon="bars-3" inset="left"/>
+    <flux:spacer/>
     <flux:dropdown position="top" align="start">
-      <flux:profile name="{{ $this->user->name }}" avatar="{{ $this->user->picture() }}" />
-      <x-user-menu />
+      <flux:profile name="{{ $this->user->name }}" avatar="{{ $this->user->picture() }}"/>
+      <x-user-menu/>
     </flux:dropdown>
   </flux:header>
 </div>
